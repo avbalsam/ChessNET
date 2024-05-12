0fab07fb70aad464d2422d006515f1d95ec268b4
Avi Balsam <avbalsam@mit.edu>
2024-05-12 16:31:52 -0400

    add files for app v2

--- app_v2/dataset.py
+++ app_v2/dataset.py
@@ -9,6 +9,100 @@ import torch.nn.functional as F
 from torch.utils.data import Dataset
 from torchvision.io import read_image
 
+from matplotlib.pyplot import figure
+from matplotlib import pyplot as plt
+import numpy as np
+from numpy import asarray
+from PIL import Image
+import cv2
+from inference import get_model
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+import os
+
+
+def order_points(pts):
+        # order a list of 4 coordinates:
+        # 0: top-left,
+        # 1: top-right
+        # 2: bottom-right,
+        # 3: bottom-left
+
+        rect = np.zeros((4, 2), dtype = "float32")
+        try:
+            s = np.sum(pts, axis = 1)
+        except Exception as e:
+            print("couldn't order pts of shape", pts.shape)
+        rect[0] = pts[np.argmin(s)]
+        rect[2] = pts[np.argmax(s)]
+        
+        diff = np.diff(pts, axis = 1)
+        rect[1] = pts[np.argmin(diff)]
+        rect[3] = pts[np.argmax(diff)]
+        
+        return rect
+
+def detect_corners(image):
+    """
+    Detects the corners of a given image. Set show_image to True to display the image with corners and intersections marked.
+    """
+    corner_model = get_model(model_id="chessnet-corner/4", api_key = "BH54WGNf3SHi0kl69JH0")
+    results = corner_model.infer( image=image.rotate(-90, expand=True), confidence=0.01, iou_threshold=0.01)[0].predictions
+    corners = np.array([(box.x, box.y) for box in results])
+    corners = order_points(corners)
+
+    return corners
+
+# perspective transforms an image with four given corners
+
+def four_point_transform(image, pts):
+
+    # img = Image.open(image)
+    image = asarray(image)
+
+    rotated_img = img.rotate(-90, expand=True)
+    image = asarray(rotated_img)
+
+    rect = order_points(pts)
+    (tl, tr, br, bl) = rect
+
+    # compute the width of the new image
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+
+
+    # compute the height of the new image
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+
+    # construct set of destination points to obtain a "birds eye view"
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype = "float32")
+
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(rect, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+    img = Image.fromarray(warped)
+    #img = Image.fromarray(warped, "RGB") #to make it black and white as original
+    # img.show()
+    # return the warped image
+    return img
+
+def perspective_transform(img):
+    pts = detect_corners(img)
+    if pts.shape != (4,2):
+        print("not transformed", pts.shape)
+        return img
+    else:
+        print("transformed")
+        return four_point_transform(img, pts)
+
 
 class ChessRecognitionDataset(Dataset):
     """ChessRecognitionDataset class.
@@ -93,6 +187,14 @@ class ChessRecognitionDataset(Dataset):
             self.images[self.images['id'] == img_id].path.values[0])
 
         img = read_image(str(img_path)).float()
+        # from torchvision.transforms import Resize, ToTensor
+        # # Add perspective transform
+        # corners = detect_corners(img_path)
+        # if len(corners) != 4:
+        #     print("bad corners")
+        # else:
+        #     img = Resize((1024, 1024))(four_point_transform(img_path, corners))
+        #     img = ToTensor()(img)
 
         if self.transform is not None:
             img = self.transform(img)
